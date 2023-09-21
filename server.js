b54cf755bb826150a45aabfbbf6415532d3f6dc2
Uaaron Sihan <uaaronsihan@gmail.com>
2023-09-21 14:59:21 -0700

    connected to Atlas database

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://127.0.0.1:27017/myflix', {useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://127.0.0.1:27017/myflix', {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true });
 
 
 // --- CORS funcitonality ---
